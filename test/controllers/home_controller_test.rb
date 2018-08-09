90d8707ce54b09cae602e4c72703057342ff01e0
ticho <tibofolly@gmail.com>
2018-08-09 22:45:24 +0200

    test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,18 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    get "/"
     assert_response :success
+  end
+
+  test "should have a refresh button" do
+    get root_path
+    assert_select 'input[value=?]', 'Refresh'
+  end
+
+  test "should have a dropdown menu" do
+    get root_path
+    assert_select 'select'
   end
 
 end
